966428952749c197d4e01b260d4a496e02560696
Luca Deri <deri@ntop.org>
2025-07-08 12:29:38 +0200

    Updated sphinx

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -170,4 +170,4 @@ texinfo_documents = [
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
